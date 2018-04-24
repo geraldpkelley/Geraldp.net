668626ccc25742cc7b274b87716a33bf77809bad
GeraldP <geraldpkelley@gmail.com>
2018-04-24 08:12:39 -0500

    Started working on click events.
    So far I have the main portfolio sections sliding in.

--- logic.js
+++ logic.js
@@ -1,2 +1,22 @@
-(function HelloWorld(){
+(function (){
+    $(".menu > li").click(MainMenuClick);
+
+    $(".portfolio-x").click(XOutClick);
+
+    function MainMenuClick(e){
+        console.log(e.currentTarget);
+
+        var whichBtn = e.currentTarget.id.replace("btn-mm", "portfolio");
+
+        $("#" + whichBtn).css({top: "0", left: "0"});
+    }
+
+    function XOutClick(e){
+        console.log(e.currentTarget);
+
+        $("#" + e.currentTarget.parentElement.parentElement.id).css({top:"", left:""});
+        console.log($(e.currentTarget.parentElement.parentElement));
+    }
+
+
 })();
