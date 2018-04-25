eeb1cfd2d1afa27483966d43386edddc58640b33
GeraldP <geraldpkelley@gmail.com>
2018-04-24 23:27:34 -0500

    Nav animations done
    Starting on flex-box

--- logic.js
+++ logic.js
@@ -1,14 +1,34 @@
 (function (){
     $(".menu > li").click(MainMenuClick);
 
+    $(".btn-gameDev, .btn-illustrator, .btn-webDesigner, .btn-aboutMe").click(NavMenuClick);
+
     $(".portfolio-x").click(XOutClick);
 
     function MainMenuClick(e){
         console.log(e.currentTarget);
 
-        var whichBtn = e.currentTarget.id.replace("btn-mm", "portfolio");
+        var whichSection = "#" +  e.currentTarget.id.replace("btn-mm", "portfolio");
 
-        $("#" + whichBtn).css({top: "0", left: "0"});
+        $(whichSection).css({top: "0", left: "0"});
+        AnimatePortfolioNav(whichSection);
+    }
+
+    function NavMenuClick(e){
+        console.log(e.currentTarget);
+
+        $("#portfolio-gameDev, #portfolio-illustrator, #portfolio-webDesigner, #portfolio-aboutMe").css({top:"", left:""});
+        var whichSection = "#" + e.currentTarget.className.replace("btn", "portfolio");
+        setTimeout(function(){
+            $(whichSection).css({top:"0", left:"0"});
+            AnimatePortfolioNav(whichSection);
+        }, 400);
+    }
+
+    function AnimatePortfolioNav(whichSection){
+        $(whichSection + " .portfolio-nav-name").addClass("portfolio-nav-nameSlideIn");
+        $(whichSection + " .portfolio-nav").addClass("portfolio-navSlideIn");
+        $(whichSection + " .socialLinks-wrapper").addClass("portfolio-nav-socialLinksSlideIn");
     }
 
     function XOutClick(e){
@@ -17,6 +37,5 @@
         $("#" + e.currentTarget.parentElement.parentElement.id).css({top:"", left:""});
         console.log($(e.currentTarget.parentElement.parentElement));
     }
-
 
 })();
