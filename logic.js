6e770a6f9ba11deccd3fade861db34bd411eb57d
GeraldP <geraldpkelley@gmail.com>
2018-05-07 13:49:31 -0500

    Finished up polishing web design. Setting up for illustrator

--- logic.js
+++ logic.js
@@ -1,4 +1,6 @@
 (function (){
+    var currentSection = "";
+
     $(".menu > li").click(MainMenuClick);
 
     $(".btn-gameDev, .btn-illustrator, .btn-webDesigner, .btn-aboutMe").click(NavMenuClick);
@@ -9,73 +11,97 @@
 
     $(".portfolio-main > .btn-x").click(XOutClick);
 
+    $(window).resize(ResizeEvent);
+    function ResizeEvent(){
+        var w = $(window).width();
+       console.log(w);
+       if (currentSection != "" && w > 1000 && !$(currentSection + " .portfolio-nav-name").hasClass(".portfolio-nav-nameSlideIn")){
+           AnimatePortfolioNav(currentSection);
+       }
+    }
+
     function MainMenuClick(e){
         console.log(e.currentTarget);
 
-        var whichSection = "#" +  e.currentTarget.id.replace("btn-mm", "portfolio");
-        var wrapper = $(whichSection + " .portfolio-nav-wrapper");
+        //which section was clicked?
+        currentSection = "#" +  e.currentTarget.id.replace("btn-mm", "portfolio");
 
-        $(whichSection).css({top: "0", left: "0"});
-        console.log(wrapper.position());
-        if (Math.floor(wrapper.position().left) == 0){  //Math.floor() is used because Firefox keeps registering very small fractions of of a pixel when 0.
-             AnimatePortfolioNav(whichSection);
+        //which wrapper is in that section?
+        var wrapper = $(currentSection + " .portfolio-nav-wrapper");
+
+        //Change the current section's position to 0,0.
+        $(currentSection).css({top: "0", left: "0"});
+
+        //If the nav's default position is out, activate menu animations.
+        if (wrapper.css("margin-left") == "0px"){
+             AnimatePortfolioNav(currentSection);
         }
     }
 
     function NavMenuClick(e){
         console.log(e.currentTarget);
 
-        var currentSection = "#" + e.currentTarget.parentElement.parentElement.parentElement.parentElement.id;
+        var previousSection = "#" + e.currentTarget.parentElement.parentElement.parentElement.parentElement.id;
 
         $("#portfolio-gameDev, #portfolio-illustrator, #portfolio-webDesigner, #portfolio-aboutMe").css({top:"", left:""});
-        var whichSection = "#" + e.currentTarget.className.replace("btn", "portfolio");
-        var wrapper = $(whichSection + " .portfolio-nav-wrapper");
+
+
+        currentSection = "#" + e.currentTarget.className.replace("btn", "portfolio");
+        
+        
+        var wrapper = $(currentSection + " .portfolio-nav-wrapper");
         setTimeout(function(){
-            $(whichSection).css({top:"0", left:"0"});
-            if (Math.floor(wrapper.position().left) == 0){
-                AnimatePortfolioNav(whichSection);
+            $(currentSection).css({top:"0", left:"0"});
+            if (wrapper.css("margin-left") == "0px"){
+                AnimatePortfolioNav(currentSection);
             }
         }, 400);
         
-        ResetNavMenuSlide(currentSection);
+        ResetNavMenuSlide(previousSection);
     }
 
-    function AnimatePortfolioNav(whichSection){
-        $(whichSection + " .portfolio-nav-name").addClass("portfolio-nav-nameSlideIn");
-        $(whichSection + " .portfolio-nav").addClass("portfolio-navSlideIn");
-        $(whichSection + " .portfolio-nav-wrapper .socialLinks-wrapper").addClass("portfolio-nav-socialLinksSlideIn");
+    function AnimatePortfolioNav(currentSection){
+        $(currentSection + " .portfolio-nav-name").addClass("portfolio-nav-nameSlideIn");
+        $(currentSection + " .portfolio-nav").addClass("portfolio-navSlideIn");
+        $(currentSection + " .portfolio-nav-wrapper .socialLinks-wrapper").addClass("portfolio-nav-socialLinksSlideIn");
     }
 
     function HamburgerClick(e){
         console.log(e.currentTarget);
 
-        var whichSection = "#" + e.currentTarget.parentElement.parentElement.id;
+        //var currentSection = "#" + e.currentTarget.parentElement.parentElement.id;
 
-        $(whichSection + " .portfolio-nav-wrapper").css({"transform": "translateX(0%)"});
-        AnimatePortfolioNav(whichSection);
+        //$(currentSection + " .portfolio-nav-wrapper").css({"transform": "translateX(0%)"});
+        $(currentSection + " .portfolio-nav-wrapper").css("margin-left", "0");
+        AnimatePortfolioNav(currentSection);
     }
 
     function NavBackClick(e){
         console.log(e.currentTarget);
 
-        var whichSection = "#" + e.currentTarget.parentElement.parentElement.parentElement.id;
+        //var currentSection = "#" + e.currentTarget.parentElement.parentElement.parentElement.id;
         
-        ResetNavMenuSlide(whichSection);
+        ResetNavMenuSlide(currentSection);
 
     }
 
     function XOutClick(e){
         console.log(e.currentTarget);
 
-        var whichSection = "#" + e.currentTarget.parentElement.parentElement.id;
+        //var currentSection = "#" + e.currentTarget.parentElement.parentElement.id;
 
-        $(whichSection).css({top:"", left:""});
+        $(currentSection).css({top:"", left:""});
         
-        ResetNavMenuSlide(whichSection);
+        ResetNavMenuSlide(currentSection);
     }
 
-    function ResetNavMenuSlide(whichSection){
-        $(whichSection + " .portfolio-nav-wrapper").css({"transform": ""});
+    function ResetNavMenuSlide(section){
+        //$(section + " .portfolio-nav-wrapper").css({"transform": ""});
+        $(section + " .portfolio-nav-wrapper").css("margin-left", "");
+    }
+
+    function CheckIfNavIsOut(whichNav){
+        
     }
 
 })();
