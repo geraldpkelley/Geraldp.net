4803d88f35d9cd8fde4f20cefd7b29ffc3d3a8cc
GeraldP <geraldpkelley@gmail.com>
2018-05-16 14:50:14 -0500

    Lots of polish stuff and started on Game Dev

--- logic.js
+++ logic.js
@@ -11,15 +11,6 @@
 
     $(".portfolio-main > .btn-x").click(XOutClick);
 
-    $(window).resize(ResizeEvent);
-    function ResizeEvent(){
-        var w = $(window).width();
-       console.log(w);
-       if (currentSection != "" && w > 1000 && !$(currentSection + " .portfolio-nav-name").hasClass(".portfolio-nav-nameSlideIn")){
-           AnimatePortfolioNav(currentSection);
-       }
-    }
-
     function MainMenuClick(e){
         console.log(e.currentTarget);
 
@@ -103,5 +94,86 @@
     function CheckIfNavIsOut(whichNav){
         
     }
+
+    //Setting up illustrator examples
+
+    //These are the file names - description pairings for each of the portfolio pieces. They will be input into urls later.
+    var illustratorPieces = [
+        "ArtBashGraphic_desktop",
+            "<span><strong>Finish Him!</strong></span><br>My submission for <strong>Bit Bash: Art Bash 2014</strong>.",
+        "GeorgeHufnagl",
+            "<strong>George Hufnagl, Sound Designer</strong>.",
+        "PaleoGame_ELPermian",
+            "<span>Another piece made for the educational game, <strong>Paleontology Manager</strong>, illustrating the Permian period in Earth's history.</span><br>The scene is of an ancient lake that used to exist in Southern Tanzania. An extinct species of dicynodonts, ancient human ancestors, are foraging in the foreground.",
+        "PaleoGame_LLPermian",
+            "<span>Another piece made for the educational game, <strong>Paleontology Manager</strong>, illustrating the Permian period in Earth's history.</span><br>The scene is of an ancient flood plain that used to exist in Southern Tanzania. In the foreground, the long since extinct species of dicynodonts, <strong>diictodon</strong>, are in conflict over a burrow.",
+        "PaleoGame_MTriassic",
+            "<span>A piece made for the educational game, <strong>Paleontology Manager</strong>, illustrating the Middle Triassic period of Earth's history.</span><br>The scene is of an ancient pond that used to exist in Southern Tanzania. A family of Asilisaurus, an early species of dinosaur, is eyeing a family of Stenaulorhynchus Stockleyi from across the water.",
+        "PaleoGame_title_desktop",
+            "<strong>Paleontology Manager</strong>'s title illustration.",
+        "Rocky_desktop",
+            "<strong>Rocky In A Blanket</strong>",
+        "WhitakerTrebella",
+            "<strong>Whitaker Trebella, Game Developer</strong>"
+    ];
+
+    var illustratorPiecesOrder = [4, 1, 2, 7, 3, 5, 6, 0];
+    var illustratorSqrPieces = [];
+
+    var illustratorElement = $("#portfolio-illustrator .portfolio-main");
+    
+    for (let i=0; i<illustratorPieces.length/4; i++){
+         var row = $("<div>", {class:"row"});
+         illustratorElement.append(row);
+    }
+    var illustratorRows = $("#portfolio-illustrator .row");
+    console.log(illustratorRows);
+
+    for (let i=0; i<illustratorPieces.length / 2; i++){
+        var whichImg = illustratorPiecesOrder[i]*2;
+        var whichDescription = illustratorPiecesOrder[i]*2+1;
+        var newFigure = $("<a>", {class:"portfolio-item", href:"#", id:illustratorPieces[whichImg]});
+        
+        illustratorRows[Math.floor(i/2)].appendChild(newFigure[0]);
+        newFigure[0].innerHTML = "<figure><img src='images/illustrator/" + illustratorPieces[whichImg] + ".jpg'><figcaption class='img-caption'>" + illustratorPieces[whichDescription] + "</figcaption></figure>";
+        if (illustratorPieces[whichImg].includes("_desktop")){
+            illustratorSqrPieces.push($("#" + illustratorPieces[whichImg] + " img"));
+        }
+    }
+
+    console.log(illustratorElement[0]);
+
+    $(window).resize(ResizeEvent);
+    function ResizeEvent(){
+        var w = $(window).width();
+       if (currentSection != "" && w > 1000 && !$(currentSection + " .portfolio-nav-name").hasClass(".portfolio-nav-nameSlideIn")){
+           AnimatePortfolioNav(currentSection);
+       }
+
+       SquareOrUnSquareImgs();
+    }
+
+    function SquareOrUnSquareImgs(){
+        if ($(window).width() > 1000 && !illustratorSqrPieces[0].attr("src").includes("_desktop")){
+            for (let i=0; i<illustratorSqrPieces.length; i++){
+                var n = illustratorSqrPieces[i].attr("src").indexOf(".jpg");
+                illustratorSqrPieces[i].attr("src", illustratorSqrPieces[i].attr("src").slice(0, n) + "_desktop.jpg");
+            }
+        } else if ($(window).width() <= 1000 && illustratorSqrPieces[0].attr("src").includes("_desktop")){
+            for (let i=0; i<illustratorSqrPieces.length; i++){
+                var n = illustratorSqrPieces[i].attr("src").indexOf("_desktop");
+                illustratorSqrPieces[i].attr("src", illustratorSqrPieces[i].attr("src").slice(0, n) + ".jpg");
+            }
+        }
+    }
+
+    $(".portfolio-item").click(TestClick);
+
+    function TestClick(e){
+        console.log(e.currentTarget);
+    }
+
+    
+    SquareOrUnSquareImgs();
 
 })();
